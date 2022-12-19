1df2231cfb8fb9b3d90bceb0d34abfd8e5c83bd6
nugrahayogaadhy <116288142+nugrahayogaadhy@users.noreply.github.com>
2022-12-19 10:27:48 +0700

    Add files via upload

---
+++ mobile_katalon/Scripts/wordpress/profile/add_url/addurl/Script1666754617256.groovy
@@ -0,0 +1,29 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.click(findTestObject('Object Repository/wordpress/coba url/Page_My Profile  WordPress.com/button_Add'))
+
+WebUI.click(findTestObject('wordpress/coba url/Page_My Profile  WordPress.com/button_Add URL'))
+
+WebUI.setText(findTestObject('Object Repository/wordpress/coba url/Page_My Profile  WordPress.com/input_Add_value'), 'jadiaddurl.com')
+
+WebUI.setText(findTestObject('Object Repository/wordpress/coba url/Page_My Profile  WordPress.com/input_Add_title'), 'jadi ')
+
+WebUI.click(findTestObject('wordpress/addurl/add_btn'))
+
